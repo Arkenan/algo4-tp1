6e5234cf0f3059325c8296f8cb100cfb3a54ad49
Tomás Arjovsky <t.arjovsky@gmail.com>
2020-10-24 20:41:21 -0300

    Añado Un par de cosas básicas de doobie

--- src/main/scala/fiuba/fp/Run.scala
+++ src/main/scala/fiuba/fp/Run.scala
@@ -1,10 +1,25 @@
 package fiuba.fp
 
+import doobie._
+import doobie.implicits._
+import cats.effect.IO
+import scala.concurrent.ExecutionContext
+
 import cats.effect._
 
 object Run extends App {
 
-    val program = IO { println(s"Hello!") }
-    
-    program.unsafeRunSync() 
+    implicit val cs = IO.contextShift(ExecutionContext.global)
+
+    val transactor = Transactor.fromDriverManager[IO](
+        "org.postgresql.Driver",
+        "jdbc:postgresql://localhost:5432/fpalgo",
+        "fiuba","password")
+
+    val q = sql"select name from fptp.movie where movie_id=1019"
+      .query[String]
+      .unique
+
+    val result = q.transact(transactor).unsafeRunSync()
+    print(s"La película es '$result'")
 }
