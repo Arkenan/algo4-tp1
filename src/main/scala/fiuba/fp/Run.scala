65fa57be01d5a1ab79fa7c8e6ec5abcf452f705f
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2020-10-29 20:18:42 -0300

    Probando lectura

--- src/main/scala/fiuba/fp/Run.scala
+++ src/main/scala/fiuba/fp/Run.scala
@@ -1,25 +1,71 @@
 package fiuba.fp
 
+import java.nio.file.Paths
+
 import doobie._
 import doobie.implicits._
 import cats.effect.IO
+
 import scala.concurrent.ExecutionContext
-
 import cats.effect._
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections
+import fs2.{Stream, io, text}
 
-object Run extends App {
+object Main extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = processFile("/home/missPanda/fiuba/ALGOIV/algo4-tp1/train.csv").compile.drain.asInstanceOf[IO[ExitCode]]
 
-    implicit val cs = IO.contextShift(ExecutionContext.global)
+  implicit val cs = IO.contextShift(ExecutionContext.global)
+/*
+  val transactor = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",
+    "jdbc:postgresql://localhost:5432/fpalgo",
+    "fiuba", "password")
 
-    val transactor = Transactor.fromDriverManager[IO](
-        "org.postgresql.Driver",
-        "jdbc:postgresql://localhost:5432/fpalgo",
-        "fiuba","password")
+  val q = sql"select * from fptp.movie"
+    .query[String].to[List]
 
-    val q = sql"select name from fptp.movie where movie_id=1019"
-      .query[String]
-      .unique
+  val result = q.transact(transactor).unsafeRunSync().take(5).foreach(println)
+*/
+  private def processFile(filePath: String): Stream[IO, Unit] = {
+    Stream.resource(Blocker[IO]).flatMap { blocker =>
+      io.file
+        .readAll[IO](Paths.get("/home/missPanda/fiuba/ALGOIV/algo4-tp1/train.csv"), blocker, 4096)
+        .through(text.utf8Decode)
+        .through(text.lines)
+        .map(line => {
+          val cols = line.split(",").map(_.trim)
+          println(cols.mkString("::"))
+        })
+    }
+      /*
+        .map(tryParseLine) // returns Option[DatasetRow]
+        .filter(option => option != Nothing) // if
+        .map(getFromDatasetRow)
+        .through(putInDB)
 
-    val result = q.transact(transactor).unsafeRunSync()
-    print(s"La película es '$result'")
+
+      println("movie_id, release_date, name, rating")
+      val bufferedSource = io.Source.fromFile()
+      bufferedSource.getLines().toStream
+      for (line <- bufferedSource.getLines) {
+          val cols = line.split(",").map(_.trim)
+          // do whatever you want with the columns here
+          println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
+      }
+      bufferedSource.close*/
+
+
+  }
+
+
 }
+
+/*
+def tryParseLine(line: String) : Option[DatasetRow] = {
+
+}
+
+  def putInDb(row : DatasetRow) : Unit = {}
+  def validate(line : String) : Boolean = {
+  true
+}*/
