7e81a2230b64d747a674123ff227e2045db02ff3
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2020-11-01 21:46:33 -0300

    Lectura y mapeo basico

--- src/main/scala/fiuba/fp/Run.scala
+++ src/main/scala/fiuba/fp/Run.scala
@@ -2,70 +2,36 @@ package fiuba.fp
 
 import java.nio.file.Paths
 
-import doobie._
-import doobie.implicits._
 import cats.effect.IO
 
-import scala.concurrent.ExecutionContext
 import cats.effect._
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections
 import fs2.{Stream, io, text}
+import models.DummyRow
 
 object Main extends IOApp {
+
   override def run(args: List[String]): IO[ExitCode] = processFile("/home/missPanda/fiuba/ALGOIV/algo4-tp1/train.csv").compile.drain.asInstanceOf[IO[ExitCode]]
 
-  implicit val cs = IO.contextShift(ExecutionContext.global)
-/*
-  val transactor = Transactor.fromDriverManager[IO](
-    "org.postgresql.Driver",
-    "jdbc:postgresql://localhost:5432/fpalgo",
-    "fiuba", "password")
 
-  val q = sql"select * from fptp.movie"
-    .query[String].to[List]
-
-  val result = q.transact(transactor).unsafeRunSync().take(5).foreach(println)
-*/
   private def processFile(filePath: String): Stream[IO, Unit] = {
     Stream.resource(Blocker[IO]).flatMap { blocker =>
       io.file
         .readAll[IO](Paths.get("/home/missPanda/fiuba/ALGOIV/algo4-tp1/train.csv"), blocker, 4096)
         .through(text.utf8Decode)
         .through(text.lines)
-        .map(line => {
-          val cols = line.split(",").map(_.trim)
-          println(cols.mkString("::"))
-        })
+        .map(mapToRow)
+        .map(row => print(row.toString))
     }
-      /*
-        .map(tryParseLine) // returns Option[DatasetRow]
-        .filter(option => option != Nothing) // if
-        .map(getFromDatasetRow)
-        .through(putInDB)
-
-
-      println("movie_id, release_date, name, rating")
-      val bufferedSource = io.Source.fromFile()
-      bufferedSource.getLines().toStream
-      for (line <- bufferedSource.getLines) {
-          val cols = line.split(",").map(_.trim)
-          // do whatever you want with the columns here
-          println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
-      }
-      bufferedSource.close*/
-
 
   }
 
+  private def mapToRow(line: String) : DummyRow = {
+
+    val cols: Array[String] = line.split(",").map(_.trim)
+    val dummy: DummyRow = DummyRow(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6), cols(7),
+                                    cols(8), cols(9), cols(10), cols(11), cols(12), cols(13), cols(14), cols(15))
+    print(dummy.toString)
+    return dummy
+  }
 
 }
-
-/*
-def tryParseLine(line: String) : Option[DatasetRow] = {
-
-}
-
-  def putInDb(row : DatasetRow) : Unit = {}
-  def validate(line : String) : Boolean = {
-  true
-}*/
---
+++ src/main/scala/fiuba/fp/models/DummyRow.scala
@@ -0,0 +1,20 @@
+package fiuba.fp.models
+
+case class DummyRow (
+                 id: String,
+                 date: String,
+                 open: String,
+                 high: String,
+                 low: String,
+                 last: String,
+                 close: String,
+                 diff: String,
+                 curr: String,
+                 OVol: String,
+                 Odiff: String,
+                 OpVol: String,
+                 unit: String,
+                 dollarBN: String,
+                 dollarItau: String,
+                 wDiff: String
+               )
