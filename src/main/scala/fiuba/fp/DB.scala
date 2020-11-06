ad57de2c79d030c212114abe50810c1ac9c44e61
Tomás Arjovsky <t.arjovsky@gmail.com>
2020-11-05 23:56:33 -0300

    DB Aesthetics 2

--- src/main/scala/fiuba/fp/DB.scala
+++ src/main/scala/fiuba/fp/DB.scala
@@ -19,6 +19,19 @@ import scala.concurrent.ExecutionContext
 
 /** Database interaction for datasets using a specified transactor. */
 case class DB(transactor : Transactor.Aux[IO, Unit]) {
+  /** Puts a dataset row in the database. */
+  def putInDb(dr: DataSetRow) : IO[Either[Throwable, Int]] = {
+    val query : fragment.Fragment =
+      sql"INSERT INTO fptp.dataset" ++
+        sql"(id, date, open, high, low, last, close, dif, curr, o_vol, o_dif, op_vol, unit, " ++
+        sql"dollar_bn, dollar_itau, w_diff, hash_code)" ++
+        sql"VALUES" ++
+        sql"(${dr.id}, ${dr.date}, ${dr.open}, ${dr.high}, ${dr.low}, ${dr.last}, ${dr.close}, ${dr.diff}," ++
+        sql"${dr.curr}, ${dr.OVol}, ${dr.Odiff}, ${dr.OpVol}, ${dr.unit}, ${dr.dollarBN}, ${dr.dollarItau}, " ++
+        sql"${dr.wDiff}, ${dr.hashCode})"
+    query.update.run.transact(transactor).attempt
+  }
+
   /** Turns the result of an insert statement into a string that can be printed into a file. */
   def toOutputLine(e: Either[Throwable, Int]) : String = {
     e match {
@@ -34,18 +47,5 @@ case class DB(transactor : Transactor.Aux[IO, Unit]) {
       last = 175, close = 3.14, diff = -750.3, curr = "A",
       OVol = Option[Int](2), Odiff = Option[Int](3), OpVol = Option[Int](0),
       unit = "US$", dollarBN = 170, dollarItau = 170.2, wDiff = 100000.0)
-  }
-
-  /** Puts a dataset row in the database. */
-  def putInDb(dr: DataSetRow) : IO[Either[Throwable, Int]] = {
-    val query : fragment.Fragment =
-      sql"INSERT INTO fptp.dataset" ++
-      sql"(id, date, open, high, low, last, close, dif, curr, o_vol, o_dif, op_vol, unit, " ++
-      sql"dollar_bn, dollar_itau, w_diff, hash_code)" ++
-      sql"VALUES" ++
-      sql"(${dr.id}, ${dr.date}, ${dr.open}, ${dr.high}, ${dr.low}, ${dr.last}, ${dr.close}, ${dr.diff}," ++
-      sql"${dr.curr}, ${dr.OVol}, ${dr.Odiff}, ${dr.OpVol}, ${dr.unit}, ${dr.dollarBN}, ${dr.dollarItau}, " ++
-      sql"${dr.wDiff}, ${dr.hashCode})"
-    query.update.run.transact(transactor).attempt
   }
 }
