fef184868e7439b93e4f0ad79aaf909642b11116
Tomás Arjovsky <t.arjovsky@gmail.com>
2020-11-06 00:08:46 -0300

    better match + autoformat

--- src/main/scala/fiuba/fp/DB.scala
+++ src/main/scala/fiuba/fp/DB.scala
@@ -18,10 +18,10 @@ import fiuba.fp.models.DataSetRow
 import scala.concurrent.ExecutionContext
 
 /** Database interaction for datasets using a specified transactor. */
-case class DB(transactor : Transactor.Aux[IO, Unit]) {
+case class DB(transactor: Transactor.Aux[IO, Unit]) {
   /** Puts a dataset row in the database. */
-  def putInDb(dr: DataSetRow) : IO[Either[Throwable, Int]] = {
-    val query : fragment.Fragment =
+  def putInDb(dr: DataSetRow): IO[Either[Throwable, Int]] = {
+    val query: fragment.Fragment =
       sql"INSERT INTO fptp.dataset" ++
         sql"(id, date, open, high, low, last, close, dif, curr, o_vol, o_dif, op_vol, unit, " ++
         sql"dollar_bn, dollar_itau, w_diff, hash_code)" ++
@@ -33,15 +33,15 @@ case class DB(transactor : Transactor.Aux[IO, Unit]) {
   }
 
   /** Turns the result of an insert statement into a string that can be printed into a file. */
-  def toOutputLine(e: Either[Throwable, Int]) : String = {
+  def toOutputLine(e: Either[Throwable, Int]): String = {
     e match {
-      case(Left(th)) => f"Error inserting row: ${th.getMessage}\n"
-      case(Right(am)) => ""
+      case (Left(throwable)) => f"Error inserting row: ${throwable.getMessage}\n"
+      case _ => ""
     }
   }
 
   /** Util function to create dummies for dataset rows with a specified ID */
-  def datasetDummy(id: Integer) : DataSetRow = {
+  def datasetDummy(id: Integer): DataSetRow = {
     DataSetRow(id = id, date = LocalDateTime.of(2020, 10, 10, 0, 0),
       open = Option[Double](700.0), high = Option[Double](200.0), low = Option[Double](150.0),
       last = 175, close = 3.14, diff = -750.3, curr = "A",
