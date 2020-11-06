fe6e40a9024532677e5f1cfe02be1708945b89b0
luciakasman <lucia.kasman@gmail.com>
2020-11-06 00:10:40 -0300

    add date validator

--- src/main/scala/fiuba/fp/Validator.scala
+++ src/main/scala/fiuba/fp/Validator.scala
@@ -1,5 +1,8 @@
 package fiuba.fp
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import fiuba.fp.models.DataSetRow
 
 import scala.util.Try
@@ -7,14 +10,19 @@ import scala.util.Try
 object Validator {
 
   def validate(id: String, date: String, open: String, high: String, low: String, last: String, close: String, dif: String, curr: String, OVol: String, ODif: String, opVol: String, unit: String, dollarBN: String, dollarItau: String, wDiff: String) : Option[DataSetRow] = {
-    Try(DataSetRow(id.toInt, date, tryToDouble(open), tryToDouble(high), tryToDouble(low), last.toDouble, close.toDouble, dif.toDouble, stringSizeValidator(curr, 1), tryToInt(OVol), tryToInt(ODif), tryToInt(opVol), stringSizeValidator(unit, 4), dollarBN.toDouble, dollarItau.toDouble, wDiff.toDouble)).toOption
+    Try(DataSetRow(id.toInt, dateValidator(date), tryToDouble(open), tryToDouble(high), tryToDouble(low), last.toDouble, close.toDouble, dif.toDouble, stringSizeValidator(curr, 1), tryToInt(OVol), tryToInt(ODif), tryToInt(opVol), stringSizeValidator(unit, 4), dollarBN.toDouble, dollarItau.toDouble, wDiff.toDouble)).toOption
   }
 
-  def stringSizeValidator(s: String, i: Int) : Option[String] = {
+  def stringSizeValidator(s: String, i: Int) : String = {
     s match {
-      case s if(s.length <= i) => Some(s)
-      case _ => None
+      case s if(s.length <= i) => s
+      case _ => throw new IllegalArgumentException
     }
+  }
+
+  def dateValidator(s: String): LocalDateTime = {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss a")
+    LocalDateTime.parse(s, formatter)
   }
 
   def tryToInt(s: String) : Option[Int] = Try(s.toInt).toOption
